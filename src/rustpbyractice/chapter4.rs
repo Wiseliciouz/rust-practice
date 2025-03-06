203bfba0d72d1e4d71e55a71281d2d159615599d
Wiseliciouz <sahandrienko@gmail.com>
2025-03-06 10:57:26 +0200

    test41 Success

--- src/rustpbyractice/chapter4.rs
+++ src/rustpbyractice/chapter4.rs
@@ -1,5 +1,12 @@
 /// https://practice.course.rs/basic-types/intro.html
 #[test]
 fn test41() {
+    let x: i32 = 5;
+    let mut y: u32 = 5;
 
+    y = x.try_into().unwrap();
+    
+    let z = 10; // Type of z ? 
+
+    println!("Success!");
 }
