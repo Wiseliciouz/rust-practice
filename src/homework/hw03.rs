a93c25fe20fdd53f2adb7a8977cb00b5cf8c188e
Wiseliciouz <sahandrienko@gmail.com>
2025-04-02 11:16:20 +0300

    Complet hw03.rs

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,14 +1,13 @@
 #[test]
 fn test3() {
-    const W: u32 = 25;
-    const H: u32 = 10;
-
+    let W: u32 = 30;
+    let H: u32 = 10;
+    let k = W as f32 / H as f32;
     for y in 0..H {
         for x in 0..W {
-            let slash  =  y + y * 2 == x + 1;
-            let slash_rev  = y + y * 2 == W - x;
-            //let slash  =  y + y * 2 == x;
-            //let slash_rev  = y + y * 2 == W - x - 1;
+            
+            let slash  = x == (y as f32 * k) as u32;
+            let slash_rev  = W  - x == (y as f32 * k) as u32;
 
             let is_hor = y == 0 || y == H - 1;
             let is_vec = x == 0 || x == W - 1;
